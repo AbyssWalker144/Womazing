5cf14e6da47ef7debee315ae5bf376cac07fafec
AbyssWalker144 <balorok5@gmail.com>
2023-04-13 17:48:32 +0300

    product img

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -10,180 +10,188 @@ import { useSelector } from 'react-redux';
 
 const Product = () => {
 
-    const { id } = useParams();
-    console.log(id);
+  const { id } = useParams();
+  console.log(id);
 
-    const womazingData = useSelector(state => state.firebaseData.value);
-    console.log(womazingData);
-    const [thisProductData, setThisProductData] = useState([]);
+  const womazingData = useSelector(state => state.firebaseData.value);
+  console.log(womazingData);
+  const [thisProductData, setThisProductData] = useState([]);
 
-    useEffect(() => {
-        if(womazingData.length !== 0) {
-            const data = womazingData.filter(item => item.id === id);
-            console.log(data);
-            setThisProductData(data[0]);
-        }
-    }, [id, womazingData]);
+  useEffect(() => {
+    if (womazingData.length !== 0) {
+      const data = womazingData.filter(item => item.id === id);
+      console.log(data);
+      setThisProductData(data[0]);
+    }
+  }, [id, womazingData]);
 
-    console.log(thisProductData);
-    console.log(thisProductData.color);
+  console.log(thisProductData);
+  console.log(thisProductData.color);
 
-    // const productData = useCatalogData("womazing", id);
-    // console.log(productData);
+  // const productData = useCatalogData("womazing", id);
+  // console.log(productData);
 
-    // const location = useLocation();
-    // const data = location.state?.data;
-    // console.log(data);
+  // const location = useLocation();
+  // const data = location.state?.data;
+  // console.log(data);
 
-    const colorsCodesObj = {
-        "ecru": "#f2e1cd",
-        "khaki": "#4a601b",
-        "orange": "#ffa500",
-        "coral-red": "#e68070",
-        "black": "#000000",
-        "green": "#008000",
-        "light-purple": "#b695c0",
-        "grey": "#808080",
-        "brown": "#3b2904",
-        "beige": "#f5f5dc",
-        "dark-blue": "#00008b",
-        "light-brown": "#C4A484",
-        "white": "#FFFFFF",
-        "pink": "#F81894",
-        "red": "#FF0000",
-        "fuchsia": "#BF026D",
-    };
+  const colorsCodesObj = {
+    "ecru": "#f2e1cd",
+    "khaki": "#4a601b",
+    "orange": "#ffa500",
+    "coral-red": "#e68070",
+    "black": "#000000",
+    "green": "#008000",
+    "light-purple": "#b695c0",
+    "grey": "#808080",
+    "brown": "#3b2904",
+    "beige": "#f5f5dc",
+    "dark-blue": "#00008b",
+    "light-brown": "#C4A484",
+    "white": "#FFFFFF",
+    "pink": "#F81894",
+    "red": "#FF0000",
+    "fuchsia": "#BF026D",
+  };
 
-    const colorsNamesFromObj = Object.keys(colorsCodesObj);
-    console.log(colorsNamesFromObj);
+  const colorsNamesFromObj = Object.keys(colorsCodesObj);
+  console.log(colorsNamesFromObj);
 
-    const [colorsOfOurProduct, setColorsOfOurProduct] = useState([]);
+  const [colorsOfOurProduct, setColorsOfOurProduct] = useState([]);
 
-    useEffect(() => {
-      if(thisProductData.length !== 0) {
-        setColorsOfOurProduct(Object.keys(thisProductData.color));
+  useEffect(() => {
+    if (thisProductData.length !== 0) {
+      setColorsOfOurProduct(Object.keys(thisProductData.color));
+    }
+  }, [thisProductData]);
+
+  console.log(colorsOfOurProduct);
+
+  let colorsObj = [];
+
+  for (let color of colorsOfOurProduct) {
+    colorTheCircle(colorsNamesFromObj, color);
+  }
+
+  function colorTheCircle(colorsNamesFromObj, color) {
+    colorsNamesFromObj.forEach(nameKey => {
+      if (color === nameKey) {
+        // console.log(`${nameKey} : ${colorsCodesObj[nameKey]}`);
+        colorsObj[nameKey] = colorsCodesObj[nameKey];
       }
-    }, [thisProductData]);
+    });
+  };
 
-    console.log(colorsOfOurProduct);
+  console.log(colorsObj);
 
-    let colorsObj = [];
+  let colorsNames = Object.keys(colorsObj);
 
-    for (let color of colorsOfOurProduct) {
-        colorTheCircle(colorsNamesFromObj, color);
-    }
+  //onClick for <li> with color:
+  function chooseColor() {
 
-    function colorTheCircle(colorsNamesFromObj, color) {
-        colorsNamesFromObj.forEach(nameKey => {
-            if (color === nameKey) {
-                // console.log(`${nameKey} : ${colorsCodesObj[nameKey]}`);
-                colorsObj[nameKey] = colorsCodesObj[nameKey];
-            }
-        });
-    };
-
-    console.log(colorsObj);
-
-    let colorsNames = Object.keys(colorsObj);
-
-    //onClick for <li> with color:
-    function chooseColor() {
-
-    }
+  }
 
 
-    const storage = getStorage();
-    const imgRef = ref(storage, `${thisProductData.mainImage}`);
+  const storage = getStorage();
+  const imgRef = ref(storage, `${thisProductData.mainImage}`);
 
-    const [imgUrl, setImgUrl] = useState('');
+  const [imgsRef, setImgsRef] = useState([]);
 
-    getDownloadURL(imgRef)
-        .then((url) => {
+  useEffect(() => {
+    const 
+  }, [womazingData])
+  
 
-            setImgUrl(url);
+  const [imgUrl, setImgUrl] = useState('');
 
-        })
-        .catch((error) => {
-            // Handle any errors
-        });
+  getDownloadURL(imgRef)
+    .then((url) => {
+
+      setImgUrl(url);
+
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
 
-    return (
-        <div>
-            <Caption caption={thisProductData.name} />
-            <div className="bread-crumbs">
-                <NavLink to="/">Главная</NavLink>
-                <span>—</span>
-                <NavLink to="/shop">Магазин</NavLink>
-                <span>—</span>
-                <NavLink to={"/shop/" + thisProductData.category}
-                // onClick={}
-                >{thisProductData.category}</NavLink>
-                <span>—</span>
-                <p>{thisProductData.name}</p>
+
+  return (
+    <div>
+      <Caption caption={thisProductData.name} />
+      <div className="bread-crumbs">
+        <NavLink to="/">Главная</NavLink>
+        <span>—</span>
+        <NavLink to="/shop">Магазин</NavLink>
+        <span>—</span>
+        <NavLink to={"/shop/" + thisProductData.category}
+        // onClick={}
+        >{thisProductData.category}</NavLink>
+        <span>—</span>
+        <p>{thisProductData.name}</p>
+      </div>
+      {/*END of bread-crumbs*/}
+
+      <div className="product">
+        <div className="product__info flex">
+          <img src={thisProductData.mainImage ? imgUrl : Item} alt={thisProductData.name} />
+
+          <div className="product__allOptions">
+            <h2 className="product__price">{thisProductData.price} грн</h2>
+
+            <h4>Размер:</h4>
+            <ul className="product__options flex">
+              {/*{data.size ?*/}
+              {/*  <>*/}
+              {/*    {data.size.map((size) => (*/}
+              {/*      <li className="product__size">{size}</li>*/}
+              {/*    ))}*/}
+              {/*  </>*/}
+              {/*  : */}
+              <li className="product__options__universal size">Универсальный размер</li>
+            </ul>
+            {/*}*/}
+
+            <h4>Цвет:</h4>
+            <ul className="product__options flex">
+
+              {
+                colorsNames.map(name => (
+                  <li style={{ backgroundColor: colorsObj[name] }} title={name} className="product__options-colorBtn"
+                  // onClick={chooseColor()}
+                  />
+                ))
+              }
+
+            </ul>
+
+            {/*<p className="product__colors__value color">Один цвет</p>*/}
+
+            <div className="product__form flex">
+              <input className="product__form__input" type="number" min='1' defaultValue='1' />
+              <button className="product__form__btn button">Добавить в корзину</button>
             </div>
-            {/*END of bread-crumbs*/}
 
-            <div className="product">
-                <div className="product__info flex">
-                    <img src={thisProductData.mainImage ? imgUrl : Item} alt={thisProductData.name} />
-
-                    <div className="product__allOptions">
-                        <h2 className="product__price">{thisProductData.price} грн</h2>
-
-                        <h4>Размер:</h4>
-                        <ul className="product__options flex">
-                            {/*{data.size ?*/}
-                            {/*  <>*/}
-                            {/*    {data.size.map((size) => (*/}
-                            {/*      <li className="product__size">{size}</li>*/}
-                            {/*    ))}*/}
-                            {/*  </>*/}
-                            {/*  : */}
-                            <li className="product__options__universal size">Универсальный размер</li>
-                        </ul>
-                        {/*}*/}
-
-                        <h4>Цвет:</h4>
-                        <ul className="product__options flex">
-
-                            {
-                                colorsNames.map(name => (
-                                    <li style={{ backgroundColor: colorsObj[name] }} title={name} className="product__options-colorBtn"
-                                    // onClick={chooseColor()}
-                                    />
-                                ))
-                            }
-
-                        </ul>
-
-                        {/*<p className="product__colors__value color">Один цвет</p>*/}
-
-                        <div className="product__form flex">
-                            <input className="product__form__input" type="number" min='1' defaultValue='1' />
-                            <button className="product__form__btn button">Добавить в корзину</button>
-                        </div>
-
-                    </div>
-                    {/*END of product__options*/}
-
-                </div>
-                {/*END of img & options of product*/}
-                <p className="product__description">{thisProductData.description}</p>
-            </div>
-            {/*END of product*/}
-
-            <h2 className="relatedProducts">Связанные товары</h2>
-            <div className="flex relatedProducts__cards">
-                {womazingData.length !== 0 && 
-                  womazingData.map(item => {
-                    if(item.category === thisProductData.category && item.id !== thisProductData.id) return <Card {...item}/>
-                  })
-                }
-            </div>
+          </div>
+          {/*END of product__options*/}
 
         </div>
-    );
+        {/*END of img & options of product*/}
+        <p className="product__description">{thisProductData.description}</p>
+      </div>
+      {/*END of product*/}
+
+      <h2 className="relatedProducts">Связанные товары</h2>
+      <div className="flex relatedProducts__cards">
+        {womazingData.length !== 0 &&
+          womazingData.map(item => {
+            if (item.category === thisProductData.category && item.id !== thisProductData.id) return <Card {...item} />
+          })
+        }
+      </div>
+
+    </div>
+  );
 };
 
 export default Product;
